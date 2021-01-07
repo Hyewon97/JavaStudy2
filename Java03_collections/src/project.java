8690f5d84fe80eecd4b162383fb23a0cbe5c7ba0
leekangsan <qnrtjd96@naver.com>
2021-01-07 18:20:43 +0900

    2020-01-07

---
+++ Java03_collections/src/project.java
@@ -0,0 +1,29 @@
+
+public class project {
+	
+	/*
+	 	사원관리
+	 	1. 로그인 기능
+	 	
+	 	로그인하면 사원목록
+	 			사원등록
+	 			사원수정
+	 			사원삭제
+	 			종료
+	 	collection로 이용 ArrayList
+	 	
+	 	2. 로그인못하면 종료(다시시작해야지 가능하게)
+	 	
+	 	3. 실행형파일 exe로 만들기
+	 	4. 팀프로젝트후 setup 만들기
+	 */
+	public project() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
