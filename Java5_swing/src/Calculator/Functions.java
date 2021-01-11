75f600324acf1021c990f9f0c5bf650d400ccc7e
leekangsan <qnrtjd96@naver.com>
2021-01-11 20:19:18 +0900

    Jframe 계산기

---
+++ Java5_swing/src/Calculator/Functions.java
@@ -0,0 +1,24 @@
+package Calculator;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class Functions implements ActionListener{
+	
+	JLabel jl;
+	JButton BC,clear,end,seven,eight,nine,plus,four,five,six,minus,one,two,three,multi,zero,point,total,division;
+	
+	public Functions() {}
+	
+	public Functions(JLabel jl, JButton BC) {
+		
+	}
+
+	
+	public void actionPerformed(ActionEvent ae) {
+	}
+
+}
