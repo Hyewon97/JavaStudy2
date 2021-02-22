2eb3f2e374620b392dc9c7aeb53adde5252bfac9
leekangsan <qnrtjd96@naver.com>
2021-02-22 19:01:53 +0900

    2021.02.22

--- java08_jdbc/src/InsertProcedure.java
+++ java08_jdbc/src/InsertProcedure.java
@@ -1,35 +1,40 @@
 import java.sql.CallableStatement;
+import java.sql.Types;
 
-public class InsertProcedure extends DBConn {
-//db에 만든 프로시져를 이용해서  db 사용하기
-	public InsertProcedure() {
-				
-		try {
-			//1. db 연결
-			getConn();
-			//2. preparedStatment
-			//		프로시저 호출
-			sql = "{call mem_insert(?, ? ,?, ?)}";
-			CallableStatement cstmt = conn.prepareCall(sql);
-			cstmt.setString(1, "배수지");
-			cstmt.setString(2, "010-7777-3333");
-			cstmt.setString(3, "suzie@naver.com");
-			cstmt.setString(4, "서울시 용산구");
-			//3. 실생
-			int resualt = cstmt.executeUpdate();
-			
-			if(resualt>0) {
-				System.out.println("회원등록 완료");
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-		}finally {
-			DBClose();
-		}
-	}
+public class InsertProcedure extends DBConn{
 
-	public static void main(String[] args) {
-		new InsertProcedure();
-	}
+   public InsertProcedure() {
+      try {
+         // 연걸
+         getConn();
+         // callablestatement 프로시저호출
+         sql="{call mem_insert(?,?,?,?,?)}";
+         // 프로시저 호출시에는 { } 를쓴다요
+         CallableStatement cstmt = conn.prepareCall(sql);
+         cstmt.setString(1, "Serah"); // 이름
+         cstmt.setString(2, "010-5716-9005"); // 연락처
+         cstmt.setString(3, "soulmate026@daum.net"); // 이메일
+         cstmt.setString(4, "경기도 부천시"); //
+         cstmt.registerOutParameter(5, Types.INTEGER);
+         
+         cstmt.executeUpdate();
+         
+         if(cstmt.getInt(5)>0) {
+            System.out.println("회원 등록 완료");
+         }else{
+        	System.out.println("회원 등록 실패");
+         }
+         
+      }catch(Exception e) {
+         e.printStackTrace();
+      } finally {
+         DBClose();
+      }
+   }
+
+   public static void main(String[] args) {
+      new InsertProcedure();
+
+   }
 
 }
--- java08_jdbc/src/UpdateProcedure.java
+++ java08_jdbc/src/UpdateProcedure.java
@@ -1,35 +1,38 @@
 import java.sql.CallableStatement;
 
-public class UpdateProcedure extends DBConn {
-//db에 만든 프로시져를 이용해서  db 사용하기
-	public UpdateProcedure() {
-		//이름을 입력받아 연락처, 이메일, 주소를 시작
-		try {
-			//1. db 연결
-			getConn();
-			//2. preparedStatment
-			//		프로시저 호출
-			sql = "{call mem_update(?, ? ,?, ?)}";
-			CallableStatement cstmt = conn.prepareCall(sql);
-			cstmt.setString(1, "이순신");
-			cstmt.setString(2, "010-2222-33333");
-			cstmt.setString(3, "ㄴㄴㄴㄴ@naver.com");
-			cstmt.setString(4, "서울시 용산구");
-			//3. 실생
-			int resualt = cstmt.executeUpdate();
-			
-			if(resualt>0) {
-				System.out.println("회원수정 완료");
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-		}finally {
-			DBClose();
-		}
-	}
+public class UpdateProcedure extends DBConn{
 
-	public static void main(String[] args) {
-		new UpdateProcedure();
-	}
+   public UpdateProcedure() {
+      // 이름을 입력받아 연락처, 이메일, 주소를 수정
+      try {
+         getConn();
+         
+         sql= "{call mem_update(?,?,?,?)}";
+         
+         CallableStatement cstmt = conn.prepareCall(sql);
+         cstmt.setString(1, "010-9913-3500");
+         cstmt.setString(2, "leculo@gmail.com");
+         cstmt.setString(3, "서울시 마포구");
+         cstmt.setString(4, "홍길동");
+         
+         int r = cstmt.executeUpdate();
+         
+         if(r>0) {
+            System.out.println("회원정보 수정이 완료되었습니다.");
+         } else {
+            System.out.println("회원정보 수정에 실패하였습니다.");
+         }
+         
+      } catch(Exception e) {
+         e.printStackTrace();
+      } finally {
+         DBClose();
+      }
+   }
+
+   public static void main(String[] args) {
+      new UpdateProcedure();
+
+   }
 
 }
